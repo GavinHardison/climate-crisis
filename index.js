734df70b4aab33aed05b936fb231b88a0ba103be
GavinHardison <147094439+GavinHardison@users.noreply.github.com>
2025-10-19 17:30:30 -0700

    added mobile formatting and index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("nav-list").style.visibility = "visible";
+    document.getElementById("navbar").style.height = "70vh";
+}
+
+function closeNav() {
+    document.getElementById("nav-list").style.visibility = "hidden";
+    document.getElementById("navbar").style.height = "9vh";
+}
